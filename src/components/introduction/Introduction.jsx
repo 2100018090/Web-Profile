b19ac044c4b5db6df1351db6b0d4abf262864be1
2100018090 <muhammad2100018090@webmail.uad.ac.id>
2026-03-29 21:37:28 +0700

    update data terbaru

--- src/components/introduction/Introduction.jsx
+++ src/components/introduction/Introduction.jsx
@@ -1,4 +1,4 @@
-import person from "../../assets/images/Foto.png";
+import person from "../../assets/images/foto_terbaru.jpeg";
 import "./introduction.css";
 import InformationSummary from "./InformationSummary";
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
